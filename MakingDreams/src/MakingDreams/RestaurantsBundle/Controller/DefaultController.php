7b44f689af5e44135a79dc5398f0f66ecd975391
cgpb89 <gpereirab89@gmail.com>
2015-06-12 03:47:51 -0600

    Creacion de una pestaña de administrador de restaurantes, en restaurante homepage

--- MakingDreams/src/MakingDreams/RestaurantsBundle/Controller/DefaultController.php
+++ MakingDreams/src/MakingDreams/RestaurantsBundle/Controller/DefaultController.php
@@ -4,13 +4,18 @@ namespace MakingDreams\RestaurantsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function showViewAction()
-    {
+class DefaultController extends Controller {
+
+    public function showViewAction() {
         return $this->render('RestaurantsBundle:Default:restaurants.html.twig');
     }
-        public function showRestAction() {
+
+    public function showRestAction() {
         return $this->render('RestaurantsBundle:Default:restaurantDetail.html.twig');
     }
+
+    public function showRestAdminAction() {
+        return $this->render('RestaurantsBundle:Default:restauranteAdministrador.html.twig');
+    }
+
 }
