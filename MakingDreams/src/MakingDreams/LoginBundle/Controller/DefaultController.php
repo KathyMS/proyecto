53b0b856cb8419dcb58d564bee43780a8373d534
cgpb89 <gpereirab89@gmail.com>
2015-06-30 14:25:03 -0600

    Login con restriccion en las vistas de package

--- MakingDreams/src/MakingDreams/LoginBundle/Controller/DefaultController.php
+++ MakingDreams/src/MakingDreams/LoginBundle/Controller/DefaultController.php
@@ -3,11 +3,42 @@
 namespace MakingDreams\LoginBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    public function showViewAction()
-    {
+class DefaultController extends Controller {
+
+    public function showViewAction() {
         return $this->render('LoginBundle:Default:login.html.twig');
     }
+
+    public function loginAction(Request $request) {
+        if ($request->getMethod() == "POST") {
+            $usuario = $request->get("email");
+            $pass = $request->get("password");
+            echo $usuario . "------------" . $pass;
+            $user = $this->getDoctrine()->getRepository('LoginBundle:Usuario')->findOneBy(array("correo" => $usuario, "contrasena" => $pass));
+
+            echo $user->getNombre();
+            if ($user) {
+                $session = $request->getSession();
+                $session->set("id", $user->getId());
+                $session->set("correo", $user->getCorreo());
+
+                if ($user->getTipo() == 1 && strcasecmp($user->getContrasena(), $pass) == 0) {
+                    return $this->redirect($this->generateUrl("packages_homepage"));
+                }
+            } else {
+                $this->get('session')->getFlashBag()->add(
+                        'mensaje', 'Los datos no son validos'
+                );
+                return $this->redirect($this->generateUrl("login_homepage"));
+            }
+        }//fin if
+        return $this->render('LoginBundle:Default:login.html.twig');
+    }
+
+//fon loginAction
 }
+
+//fin class
+    
--- MakingDreams/src/MakingDreams/PackagesBundle/Controller/DefaultController.php
+++ MakingDreams/src/MakingDreams/PackagesBundle/Controller/DefaultController.php
@@ -3,22 +3,65 @@
 namespace MakingDreams\PackagesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller {
 
-    public function showViewAction() {
-        return $this->render('PackagesBundle:Default:packages.html.twig');
+    public function showViewAction(Request $request) {
+
+        $session = $request->getSession();
+
+        if ($session->has("id")) {
+            return $this->render('PackagesBundle:Default:packages.html.twig');
+        } else {
+            $this->get('session')->getFlashBag()->add(
+                    'mensaje', 'Debe estar logueado para ver paquetes'
+            );
+
+            return $this->redirect($this->generateUrl('login_homepage'));
+        }
     }
 
     public function showPackageAction() {
-        return $this->render('PackagesBundle:Default:package.html.twig');
+        $session = $request->getSession();
+
+        if ($session->has("id")) {
+            return $this->render('PackagesBundle:Default:package.html.twig');
+        } else {
+            $this->get('session')->getFlashBag()->add(
+                    'mensaje', 'Debe estar logueado para ver paquetes'
+            );
+
+            return $this->redirect($this->generateUrl('login_homepage'));
+        }
     }
 
     public function showNewPackageAction() {
-        return $this->render('PackagesBundle:Default:new_package.html.twig');
+        $session = $request->getSession();
+
+        if ($session->has("id")) {
+            return $this->render('PackagesBundle:Default:new_package.html.twig');
+        } else {
+            $this->get('session')->getFlashBag()->add(
+                    'mensaje', 'Debe estar logueado para ver paquetes'
+            );
+
+            return $this->redirect($this->generateUrl('login_homepage'));
+        }
     }
 
     public function showEditPackageAction() {
-        return $this->render('PackagesBundle:Default:edit_package.html.twig');
+        $session = $request->getSession();
+
+        if ($session->has("id")) {
+            return $this->render('PackagesBundle:Default:edit_package.html.twig');
+        } else {
+            $this->get('session')->getFlashBag()->add(
+                    'mensaje', 'Debe estar logueado para ver paquetes'
+            );
+
+            return $this->redirect($this->generateUrl('login_homepage'));
+        }
     }
+
 }
