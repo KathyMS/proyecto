60082836b16c43aa5e83079740511f48c5a0abf8
kathyms <kathymors11@gmail.com>
2015-07-02 12:32:53 -0600

    Subir imagenes a la base en cruds de paquete y restaurante. Cambio de routing en estos bundles y creacion carpeta uploads para guardar imagenes cargadas

--- MakingDreams/src/MakingDreams/PackagesBundle/Controller/PaqueteController.php
+++ MakingDreams/src/MakingDreams/PackagesBundle/Controller/PaqueteController.php
@@ -4,7 +4,6 @@ namespace MakingDreams\PackagesBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use MakingDreams\PackagesBundle\Entity\Paquete;
 use MakingDreams\PackagesBundle\Form\PaqueteType;
 
@@ -12,30 +11,99 @@ use MakingDreams\PackagesBundle\Form\PaqueteType;
  * Paquete controller.
  *
  */
-class PaqueteController extends Controller
-{
+class PaqueteController extends Controller {
 
     /**
      * Lists all Paquete entities.
      *
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('PackagesBundle:Paquete')->findAll();
 
         return $this->render('PackagesBundle:Paquete:index.html.twig', array(
-            'entities' => $entities,
+                    'entities' => $entities,
         ));
     }
-    
+
     /**
      * Creates a new Paquete entity.
      *
      */
-    public function createAction(Request $request)
-    {
+    public function editarAction($id) {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PackagesBundle:Paquete')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Paquete entity.');
+        }
+
+        return $this->render('PackagesBundle:Paquete:editar.html.twig', array(
+                    'entity' => $entity));
+    }
+
+//editar
+
+    public function imagenAction($id) {
+//IMAGEN
+        $root = getcwd();
+        $target_dir = $root . '/uploads/';
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+        if (isset($_POST["submit"])) {
+//            $valor = $_POST["text"];
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+// Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+// Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+// Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+// Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+
+                $em = $this->getDoctrine()->getManager();
+                $paquete = $em->getRepository('PackagesBundle:Paquete')->find($id);
+                $paquete->setImagen($target_file);
+
+                $em->persist($paquete);
+                $em->flush();
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+        return $this->render('PackagesBundle:Paquete:editar.html.twig');
+    }
+
+//image
+
+    public function createAction(Request $request) {
         $entity = new Paquete();
         $form = $this->createCreateForm($entity);
         $form->handleRequest($request);
@@ -49,8 +117,8 @@ class PaqueteController extends Controller
         }
 
         return $this->render('PackagesBundle:Paquete:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
+                    'entity' => $entity,
+                    'form' => $form->createView(),
         ));
     }
 
@@ -61,8 +129,7 @@ class PaqueteController extends Controller
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createCreateForm(Paquete $entity)
-    {
+    private function createCreateForm(Paquete $entity) {
         $form = $this->createForm(new PaqueteType(), $entity, array(
             'action' => $this->generateUrl('paquete_create'),
             'method' => 'POST',
@@ -77,14 +144,13 @@ class PaqueteController extends Controller
      * Displays a form to create a new Paquete entity.
      *
      */
-    public function newAction()
-    {
+    public function newAction() {
         $entity = new Paquete();
-        $form   = $this->createCreateForm($entity);
+        $form = $this->createCreateForm($entity);
 
         return $this->render('PackagesBundle:Paquete:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
+                    'entity' => $entity,
+                    'form' => $form->createView(),
         ));
     }
 
@@ -92,8 +158,7 @@ class PaqueteController extends Controller
      * Finds and displays a Paquete entity.
      *
      */
-    public function showAction($id)
-    {
+    public function showAction($id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('PackagesBundle:Paquete')->find($id);
@@ -105,8 +170,8 @@ class PaqueteController extends Controller
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('PackagesBundle:Paquete:show.html.twig', array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
+                    'entity' => $entity,
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -114,8 +179,7 @@ class PaqueteController extends Controller
      * Displays a form to edit an existing Paquete entity.
      *
      */
-    public function editAction($id)
-    {
+    public function editAction($id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('PackagesBundle:Paquete')->find($id);
@@ -128,21 +192,20 @@ class PaqueteController extends Controller
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('PackagesBundle:Paquete:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
 
     /**
-    * Creates a form to edit a Paquete entity.
-    *
-    * @param Paquete $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Paquete $entity)
-    {
+     * Creates a form to edit a Paquete entity.
+     *
+     * @param Paquete $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(Paquete $entity) {
         $form = $this->createForm(new PaqueteType(), $entity, array(
             'action' => $this->generateUrl('paquete_update', array('id' => $entity->getId())),
             'method' => 'PUT',
@@ -152,12 +215,12 @@ class PaqueteController extends Controller
 
         return $form;
     }
+
     /**
      * Edits an existing Paquete entity.
      *
      */
-    public function updateAction(Request $request, $id)
-    {
+    public function updateAction(Request $request, $id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('PackagesBundle:Paquete')->find($id);
@@ -177,17 +240,17 @@ class PaqueteController extends Controller
         }
 
         return $this->render('PackagesBundle:Paquete:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
+
     /**
      * Deletes a Paquete entity.
      *
      */
-    public function deleteAction(Request $request, $id)
-    {
+    public function deleteAction(Request $request, $id) {
         $form = $this->createDeleteForm($id);
         $form->handleRequest($request);
 
@@ -213,13 +276,13 @@ class PaqueteController extends Controller
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm($id)
-    {
+    private function createDeleteForm($id) {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('paquete_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete', 'attr' => array('class' => 'btn btn-primary')))
-            ->getForm()
+                        ->setAction($this->generateUrl('paquete_delete', array('id' => $id)))
+                        ->setMethod('DELETE')
+                        ->add('submit', 'submit', array('label' => 'Delete', 'attr' => array('class' => 'btn btn-primary')))
+                        ->getForm()
         ;
     }
+
 }
